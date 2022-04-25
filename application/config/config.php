646f7cbd8daca7b2110f35fea73862d96bb8933a
yanribas <yan.ribeiro16@hotmail.com>
2022-04-25 14:00:03 -0300

    Troca de baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/self_page/';
+//$config['base_url'] = 'http://localhost/self_page/';
+$config['base_url'] = 'https://yanribas.github.io/yanselfpage/';
 
 /*
 |--------------------------------------------------------------------------
