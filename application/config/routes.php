fbb39a6c099cccb44143337bb98e186114aab1f4
Yan Ribeiro <yan.ribeiro16@hotmail.com>
2022-08-17 21:22:10 -0300

    correções
    Removendo a página para correção

--- application/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-$route['default_controller'] = 'home';
-$route['home/index'] = 'home/index';
-$route['translate_uri_dashes'] = FALSE;
